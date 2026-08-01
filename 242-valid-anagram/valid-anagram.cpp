cdc5e30c4a3c250b46e436c1f718f635e1693fe3
V.WILLIAM GLADSTON <williamgladston4@gmail.com>
2026-08-02 03:46:22 +0530

    Time: 10 ms (6.99%) | Memory: 9.8 MB (50.14%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char,int>mp1 ,mp2;
+        int n1 = s.size();
+        int n2 = t.size();
+        if(n1!=n2)return false;
+        for(int i = 0 ; i < n1;i++){
+            mp1[s[i]]++;
+            mp2[t[i]]++;
+        }
+        return mp1==mp2;
+    }
+};
